1d646cf1e9aa1beb182d51fe6f14aeea210ebb81
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-11-30 17:41:46 -0500

    config project

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "hel"
+import os
+from app import app
 
 if __name__ == "__main__":
-    app.run()
+    app.debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
+    
+    host = os.environ.get('IP' , '127.0.0.1')
+    port = int(os.environ.get('PORT', 5000))
+
+    try:
+        print(f"Starting server at http://{host}:{port} (Debug={app.debug})")
+        app.run(host=host, port=port)
+    except Exception as e:
+        print(f"Error: {e}")
