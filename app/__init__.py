8d6cfea5351297858521b0dfd2456e14b9f182a7
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-12-10 09:01:21 -0500

    start bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,9 @@ def create_app(testing_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     from .views import views
-
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth)
 
     return app
--- app/views.py
+++ app/views.py
@@ -8,12 +8,3 @@ def home():
     fruits = ['apple', 'banana', 'melon', 'watermelon']
     return render_template('index.html', fruits=fruits)
 
-
-@views.route('/login')
-def login():
-    return render_template('login.html')
-
-
-@views.route('/register')
-def register():
-    return render_template('register.html')
