3623834df4374f927b2f4296d64794e57ee36f04
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-12-12 15:07:37 -0500

    register modifed

--- app/auth.py
+++ app/auth.py
@@ -10,11 +10,23 @@ def login():
     return render_template('login.html', text="Helo")
 
 
-@auth.route('/register')
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        print(request.form)
+        # get info
+        user1 = request.form.get('user1')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if password1 != password2:
+            return 'Incorret password, try again!'
+        print(f'{user1}, {email}, {password1}, {password2}')
+
     return render_template('register.html')
 
 
-@auth.route('/logout', methods=['GET', 'POST'])
+@auth.route('/logout')
 def logout():
     return 'logout'
