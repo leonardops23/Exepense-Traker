ba60ff111081dc4fa4d5ffab1987e18b93595e16
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-12-12 22:20:20 -0500

    creating modelds

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(testing_config=None):
     app = Flask(__name__, instance_relative_config=True)
+
+    app.config['SECRET_KEY'] = 'asdjfklj!@!2ssdkj'
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
--- app/auth.py
+++ app/auth.py
@@ -1,32 +1,34 @@
-from flask import Blueprint, render_template, request
-
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     data = request.form
-    print(data)
-    return render_template('login.html', text="Helo")
-
+    print(data)  # Depuración: Imprime los datos del formulario en la consola
+    return render_template('login.html', text="Hello")
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        print(request.form)
-        # get info
-        user1 = request.form.get('user1')
+        username = request.form.get('user1')
         email = request.form.get('email')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if password1 != password2:
-            return 'Incorret password, try again!'
-        print(f'{user1}, {email}, {password1}, {password2}')
+        if len(username) < 2:
+            flash('Username must be longer than 1 character.', 'danger')
+        elif len(email) == 0:
+            flash('please fill in the fields', 'danger')
+        elif password1 != password2:
+            flash('Passwords do not match.', 'danger')
+        elif len(password1) < 6:
+            flash('Password must be at least 6 characters long.', 'danger')
+        else:
+            flash('Registration successful!', 'success')
 
     return render_template('register.html')
 
-
 @auth.route('/logout')
 def logout():
-    return 'logout'
+    return 'You have been logged out.'
